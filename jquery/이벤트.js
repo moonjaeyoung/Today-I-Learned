096b99ec3d67aed4d21427583b99dcf16e9a39f1
LAPTOP-5R57ERUD\Administrator <gusdnd852@naver.com>
2018-09-08 10:30:54 +0900

    제이쿼리, 버튼이벤트

---
+++ jquery/이벤트.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $(".btn1").click(function () {
+        alert("1번 클릭");
+    });
+    $(".btn2").click(function () {
+        alert("2번 클릭");
+    });
+    $(".btn3").click(function () {
+        alert("3번 클릭");
+    });
+
+});
---
+++ jquery/제이쿼리.js
@@ -0,0 +1,25 @@
+$(function () {
+    $(".wrap_1").css("border", "1px solid orange");
+    $(".text").css("background-color", "aqua");
+    // 직접 선택자
+
+    $(".wrap_1 p").eq(2).css("background-color", "yellow");
+    // 위치 선택자
+
+    $("input[type=text]").css({
+        "background-color": "orange"
+    });
+    //속성 선택자
+    //여러개의 속성을 주려면 객체형으로 (파이썬 딕셔너리식으로)
+
+    let num = prompt("숫자를 입력하세요.");
+
+    if (num > 5) {
+        $("input[type=text]").remove();
+        if (num > 100) {
+            $(".wrap_1").removeClass("text");
+        }
+    }else {
+        $(".wrap_1 p").append("<p>텍스트2</p>")
+    }
+});
