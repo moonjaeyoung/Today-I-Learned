062b4d3c4e65e15e588a6e0f645795c2d13d5cb7
LAPTOP-5R57ERUD\Administrator <gusdnd852@naver.com>
2018-09-08 16:50:34 +0900

    톰캣 연동

---
+++ jquery/비동기.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $.ajax({
+        url: "MOCK_DATA.json",
+        dataType: "json",
+        success: function (data) {
+            document.write(data);
+        },
+        error: function () {
+            document.write("에러 발생")
+        }
+    });
+
+});
+//ajax 기본형
