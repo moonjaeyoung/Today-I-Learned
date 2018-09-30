cabc5542cefa92ac453d9c8282b6328308bc16b7
LAPTOP-5R57ERUD\Administrator <gusdnd852@naver.com>
2018-09-30 13:23:52 +0900

    로지스틱 회귀분석

---
+++ MachineLearning/linier_regression/load_data_1.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+tf.set_random_seed(777)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+file = np.loadtxt('data.csv', delimiter=',', dtype=np.float32)
+
+X_data = file[:, :3]
+Y_data = np.array(file[:, 3]).reshape(-1, 1)
+# N x 1 의 행렬로 만들어줘야함.
+
+X = tf.placeholder(tf.float32, shape=[None, 3])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+
+W = tf.Variable(tf.random_normal([3, 1]))
+b = tf.Variable(tf.random_normal([1]))
+
+hypothesis = tf.matmul(X, W) + b
+cost = tf.reduce_min(tf.square(hypothesis - Y))
+
+train = tf.train.GradientDescentOptimizer(learning_rate=1e-6).minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+# TRAINING SESSION !!
+# for i in range(0, 3001):
+#     c, h, w, t = sess.run([cost, hypothesis, W, train], feed_dict={X: X_data, Y: Y_data})
+#
+#     print("cost : \n", c)
+#     print("weight : \n", w)
+#     print("hypothesis : \n", h)
+#     print("\n")
+
+# weight = 1.387192 , 1.2015423 , -0.6114366
+
+W_trained = [1.387192, 1.2015423, -0.6114366]
+W_trained_reshape = np.array(W_trained, dtype=float).reshape(1, 3)
+
+X_prediction = np.array([68, 78, 72])
+result = W_trained_reshape.dot(X_prediction)
+print(result)
---
+++ MachineLearning/logistic_regression/logistic_regression.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import numpy as np
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+file = np.loadtxt('data.csv')
+X_data = np.array(file[:, :5], dtype=np.float32)  # 5 x N
+Y_data = np.array(file[:, 5], dtype=np.float32).reshape(-1, 1)  # N x 1
+
+X = tf.placeholder(tf.float32, shape=[None, 5])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+W = tf.Variable(tf.random_normal([5, 1]), dtype=tf.float32)
+
+hypothesis = tf.div(1., 1. + tf.exp(-tf.matmul(X, W)))
+# tf.sigmoid(tf.matmul(X, W)) 이걸로 써도 됨.
+
+cost = tf.reduce_min(-Y * tf.log(hypothesis) - (1 - Y) * tf.log(1 - hypothesis))
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for i in range(0, 5001):
+    h_, c_, w_, _ = sess.run([hypothesis, cost, W, train], feed_dict={X: X_data, Y: Y_data})
+    print(i, "번째 학습 hypothesis : \n", h_)
+    print(i, "번째 학습 cost : \n", c_)
+    print(i, "번째 학습 W : \n", w_)
+    print("\n")
