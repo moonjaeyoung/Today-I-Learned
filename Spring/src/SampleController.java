0e8562b786f650f42f3e775489f7a01f93ab1e57
LAPTOP-5R57ERUD\Administrator <gusdnd852@naver.com>
2018-10-06 01:56:26 +0900

    no message

---
+++ Spring/src/SampleController.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author : Hyunwoong
+ * @When : 2018-10-06 오전 1:55
+ * @Homepage : https://github.com/gusdnd852
+ */
+@RestController
+public class SampleController {
+
+}
